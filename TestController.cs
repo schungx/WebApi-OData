e49dd6fd6ff3a413db1ac46e18c4dd0410984a2d
Stephen Chung <Stephen.Chung@intexact.com>
2014-07-29 19:48:38 +0800

    Updated TestController

--- TestController.cs
+++ TestController.cs
@@ -8,12 +8,12 @@ namespace WebApi.OData.Controllers
 		public string Message { get; set; }
 	}
 
-	public class TestController : StandardODataControllerBase<HomeNetDB>
+	public class TestController<DB> : StandardODataControllerBase<DB>
 	{
 		[EnableQuery]
 		public IEnumerable<Test> Get ()
 		{
-			yield return new Test() { Message = "The OData server is on-line. The time is " + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss tt") + "." };
+			yield return new Test() { Message = "The OData server is on-line. The local time is " + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss tt") + "." };
 		}
 	}
 }
